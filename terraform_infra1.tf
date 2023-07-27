a69a6ae33bb354e62b53c537216e6262c8299f41
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-27 13:29:40 -0700

    Update terraform_infra1.tf

--- terraform_infra1.tf
+++ terraform_infra1.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 1.5.4." 
+      version = "~> 1.5.4" 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "mytest_terraform" {
-  ami           = "ami-03f65b8614a860c29"
+  ami = "ami-03f65b8614a860c29"
   instance_type = "t3.micro"
 
   tags = {
