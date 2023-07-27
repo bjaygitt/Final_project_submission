7f7fafc4ded07d8c066cee561386ec3e67b95b10
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-27 13:51:10 -0700

    Update terraform_infra1.tf

--- terraform_infra1.tf
+++ terraform_infra1.tf
@@ -1,19 +1,24 @@
 terraform {
+  required_version = ">= 0.12"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 1.5.4" 
+      source  = "hashicorp/aws"
+      version = "~> 1.5.4"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "mytest_terraform" {
-  ami = "ami-03f65b8614a860c29"
+  ami           = "ami-03f65b8614a860c29"
   instance_type = "t3.micro"
-
+  
   tags = {
     Name = "myterrademo"
   }
+  
   count = 3
 }
-
