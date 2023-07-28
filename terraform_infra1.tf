8782cc77a6984669d7b85adb25f1469f87d32272
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-28 03:30:27 -0700

    Update terraform_infra1.tf

--- terraform_infra1.tf
+++ terraform_infra1.tf
@@ -10,8 +10,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  access_key = "var.access_key"
+  secret_key = "var.secret_key"
 }
-
 resource "aws_instance" "mytest_terraform" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t3.micro"
