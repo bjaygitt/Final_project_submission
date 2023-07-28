7043356a46d0ba2cb6a9b0ebfc73c9863fbb489c
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-28 13:28:10 -0700

    Update terraform_infra1.tf

--- terraform_infra1.tf
+++ terraform_infra1.tf
@@ -3,9 +3,6 @@ provider "aws" {
  shared_credential_files = ["~/.aws/credentials"]
  profile = "default"
 }
-  access_key = "var.access_key"
-  secret_key = "var.secret_key"
-}
 resource "aws_instance" "mytest_terraform" {
   ami = "ami-03f65b8614a860c29"
   instance_type = "t3.micro"
