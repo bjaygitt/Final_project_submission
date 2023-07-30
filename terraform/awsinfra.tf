8c990bceb201aaaecc4f2cdb4306962816d15acb
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-29 18:07:29 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -2,8 +2,8 @@
 
 provider "aws" {
   region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  shared_credential_files = "~/.aws/credentials"
+  profile = "default"
 }
 provider "aws" {
   region = "us-east-1"
