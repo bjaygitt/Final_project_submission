8c091a145e7337a74801c17b5f6ed702afbe1db1
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-30 08:24:10 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
-  shared_credentials_files = "/home/ubuntu/.aws/credentials"
-  profile = "default"
+  region                  = "us-east-1"
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  profile                 = "default"
 }
 
 resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  count = 3
+  count         = 3
   tags = {
     Name = "${var.env}-${count.index}"
   }
