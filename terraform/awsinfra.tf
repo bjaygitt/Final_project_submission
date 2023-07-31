93f1608dc4cd9c5d8af31b4acf0c5e833d61a9cf
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-07-31 14:36:07 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -1,42 +1,18 @@
-
 provider "aws" {
   region = "us-east-1"
   shared_credential_files =["~/.aws/credentials"]
   profile = "default"
 }
 
-variable "sg_ports" {
-  type        = list(number)
-  description = "list of ingress ports"
-  default     = [8200, 8201, 8300, 9200, 9500]
-
-
 resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = var.instance_type
 
   count = 3
+
   tags  = {
     Name = "${var.env}-${count.index}"
   }
 }
 
-resource "aws_security_group" "my_sg" {
-  name        = "my_security_group"
-  description = "Dynamic block"
-
-    dynamic "ingress"{
-    for_each = var.sg_ports
-    iterator = port
-    content {
-      from_port        = port.value
-      to_port          = port.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-   }
-  }
-
-}
-
-
-                                                       2,1 
+    
