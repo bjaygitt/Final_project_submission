187e2ecd1a62ec7fe52e15d0bba5c93f18b7b631
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-08-07 18:56:48 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -1,7 +1,7 @@
 provider "aws" {
  region     = "us-east-1"
- shared_credentials_files = ["~/.aws/credentials"]
- profile = "default"
+ shared_credentials_files = ["%USERPROFILE%/.aws/credentials"]
+ profile = "customprofile"
 }
 
 resource "aws_instance" "myec2" {
