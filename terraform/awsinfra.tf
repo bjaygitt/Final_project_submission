153b6f46ee3b4822b08df7536b1484261a95d174
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-08-17 02:26:29 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "myec2" {
 
 resource "aws_instance" "myec2" {
   ami           = var.ami
-  instance_type1 = var.instance_type1
+  instance_type_1 = var.instance_type_1
 
   count = 1
 
