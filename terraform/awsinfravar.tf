66863db5832101efc022e4506514abda56c98845
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-08-17 01:53:36 -0700

    Update awsinfravar.tf

--- terraform/awsinfravar.tf
+++ terraform/awsinfravar.tf
@@ -11,4 +11,19 @@ default = "t3.micro"
 variable "env" {
 
 default = "dev"
+}variable "ami" {
+
+default = "ami-0ea18256de20ecdfc"
 }
+
+variable "instance_type" {
+
+default = "t3.micro"
+
+}
+variable "env" {
+
+default = "prod"
+}
+
+
