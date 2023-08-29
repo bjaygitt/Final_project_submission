cf6587cb029777814e4544b338d2b000331f574a
Timojeaga <115139508+Timojeaga@users.noreply.github.com>
2023-08-28 20:49:54 -0700

    Update awsinfra.tf

--- terraform/awsinfra.tf
+++ terraform/awsinfra.tf
@@ -14,5 +14,15 @@ resource "aws_instance" "myec2" {
   tags = {
     env  = var.env
     Name = "${var.env}-${count.index}"
+
+variable "key_name" {
+  description = "Name of the default SSH key pair"
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = var.key_name
+  public_key = file("/home/ubuntu/.ssh/id_rsa.pub") # Replace with the path to your public key
+}
+
   }
 }
